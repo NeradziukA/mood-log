6836afb7d4250f42db62ca99a3d3e529913bc6ba
Aliaksandr Neradziuk <aliaksandr_neradziuk@epam.com>
2023-05-08 15:48:23 +0200

    add function url

--- src/MoodSelector/index.js
+++ src/MoodSelector/index.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./moodSelector.module.css";
 
 const MoodSelector = () => {
   const [selectedMood, setSelectedMood] = useState("");
+  const [allMoods, setAllMoods] = useState([]);
+
+  useEffect(() => {
+    handleMoodLoading();
+  }, []);
+
+  const handleMoodLoading = async () => {
+    try {
+      const response = await axios.get(
+        "https://mood-logs-functions.azurewebsites.net/api/mood-logs"
+      );
+
+      setAllMoods(response.data); // Optional: Handle success response
+    } catch (error) {
+      console.error(error); // Optional: Handle error response
+    }
+  };
 
   const handleMoodSelection = async (mood) => {
     setSelectedMood(mood);
 
     try {
       const response = await axios.post(
-        "https://your-azure-function-url",
-        { mood },
+        "https://mood-logs-functions.azurewebsites.net/api/mood-logs",
+        { name: mood },
         { withCredentials: true } // Include this line if you need to send authentication cookies along with the request
       );
 
@@ -57,6 +74,12 @@ const MoodSelector = () => {
         </button>
       </div>
       {selectedMood && <p>Your current mood: {selectedMood}</p>}
+      {allMoods && (
+        <p>
+          Your mood history:
+          {JSON.stringify(allMoods.map((m) => `${m.mood} - ${m.id}`))}
+        </p>
+      )}
     </div>
   );
 };
